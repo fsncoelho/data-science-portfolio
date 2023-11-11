95e68379428b6227c82c8f7a7986451acb854101
Felipe Coelho <felipesn.coelho@gmail.com>
2023-11-11 19:30:57 -0300

    Add exercises

---
+++ ebac_modules/analise.py
@@ -0,0 +1,12 @@
+# Preamble
+from sys import argv
+import extracao 
+import visualizacao
+
+# Roda extracao de cdi
+extracao
+
+# salva grafico com nome via argv[1]
+visualizacao
+
+print("That's all folks!")
